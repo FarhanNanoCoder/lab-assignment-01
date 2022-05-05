ab3fc1e3dad782570ed6a385a84c43ad2f1a116c
FarhanNanoCoder <farhan.shariar.094@gmail.com>
2022-05-05 23:57:08 +0600

    added Test1.java

---
+++ src/Test1.java
@@ -0,0 +1,25 @@
+class Test1{
+    private String name;
+    private int age;
+
+    public Test1(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
